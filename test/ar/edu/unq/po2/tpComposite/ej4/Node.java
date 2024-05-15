377c10e6d702dc3d02c26060358a65c40edf6fd9
LautaroLovato <lautaro.lovato07@gmail.com>
2024-05-15 20:07:56 -0300

    create class Node

---
+++ test/ar/edu/unq/po2/tpComposite/ej4/Node.java
@@ -0,0 +1,16 @@
+package ar.edu.unq.po2.tpComposite.ej4;
+
+abstract class Node {
+	
+	String name;
+	
+	public Node(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	
+}
